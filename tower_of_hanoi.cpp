70112af262c23d00b9f11299097e30348615f4bf
Anuska Acharya <acharyaanuska111@gmail.com>
2026-06-07 08:40:22 +0545

    Add files via upload

---
+++ tower_of_hanoi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void toh(int n, char a, char b, char c)
+{
+    if (n == 1)
+    {
+        cout << "Move disk 1 from " << a << " to " << c << endl;
+        return;
+    }
+
+    toh(n - 1, a, c, b);
+
+    cout << "Move disk " << n << " from " << a << " to " << c << endl;
+
+    toh(n - 1, b, a, c);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of disks: ";
+    cin >> n;
+
+    toh(n, 'A', 'B', 'C');
+
+    return 0;
+}
+   
